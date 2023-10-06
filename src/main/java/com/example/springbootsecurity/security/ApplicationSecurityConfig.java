f0bfc82d5d9045b18c443606e4a3897f3fd0b884
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-10-06 11:20:10 +0300

    basic auth

---
+++ src/main/java/com/example/springbootsecurity/security/ApplicationSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.springbootsecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig {
+      @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+          http
+                  .authorizeHttpRequests((authz) -> authz
+                          .anyRequest().authenticated()
+                  )
+                  .httpBasic(withDefaults());
+          return http.build();
+      }
+
+    }
+
