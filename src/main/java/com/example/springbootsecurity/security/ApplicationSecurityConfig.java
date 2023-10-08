768d9fd61ea4177c879a727d991bd584c87cd444
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-10-08 10:30:28 +0300

    in memory user details manager

--- src/main/java/com/example/springbootsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springbootsecurity/security/ApplicationSecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -21,9 +25,24 @@ public class ApplicationSecurityConfig {
                           .permitAll()
                           .anyRequest()
                           .authenticated()
+                          
                   )
                   .httpBasic(withDefaults());
           return http.build();
+      }
+
+//    @Override
+    @Bean
+    protected UserDetailsService userDetailsService(){
+          UserDetails wycliffeMwimaliUser = User.builder()
+                  .username("wycliffemwimali")
+                  .password("mwimali")
+                  .roles("STUDENT")
+                  .build();
+
+          return new InMemoryUserDetailsManager(
+                  wycliffeMwimaliUser
+          );
       }
 
     }
