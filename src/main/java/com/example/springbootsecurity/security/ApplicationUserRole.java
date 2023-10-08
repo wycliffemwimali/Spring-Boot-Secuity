32f1dd135f482d6a834732994440740eaa743ed7
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-10-08 11:25:53 +0300

    added permissions to roles

--- src/main/java/com/example/springbootsecurity/security/ApplicationUserRole.java
+++ src/main/java/com/example/springbootsecurity/security/ApplicationUserRole.java
@@ -1,6 +1,22 @@
 package com.example.springbootsecurity.security;
 
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+import static com.example.springbootsecurity.security.ApplicationUserPermission.*;
+
 public enum ApplicationUserRole {
-    STUDENT,
-    ADMIN
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE, STUDENT_WRITE, STUDENT_READ));
+
+    public final Set<ApplicationUserPermission> permissions;
+
+    ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
 }
