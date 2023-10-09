37ccd4ffcf5d8d218daf189fdac4b230d5dd5a32
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-10-09 10:53:02 +0300

    permission based auth

--- src/main/java/com/example/springbootsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springbootsecurity/security/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.springbootsecurity.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -12,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static com.example.springbootsecurity.security.ApplicationUserPermission.*;
 import static com.example.springbootsecurity.security.ApplicationUserRole.*;
 import static com.example.springbootsecurity.security.ApplicationUserRole.STUDENT;
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -29,10 +31,15 @@ public class ApplicationSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
           http
+                  .csrf().disable()
                   .authorizeHttpRequests((authz) -> authz
                           .requestMatchers("/", "index", "/css/*", "/js/*")
                           .permitAll()
                           .requestMatchers("/api/**").hasRole(STUDENT.name())
+                          .requestMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.name())
+                          .requestMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.name())
+                          .requestMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.name())
+                          .requestMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                           .anyRequest()
                           .authenticated()
                           
--- src/main/java/com/example/springbootsecurity/student/Student.java
+++ src/main/java/com/example/springbootsecurity/student/Student.java
@@ -17,4 +17,11 @@ public class Student {
     public String getStudentName() {
         return studentName;
     }
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId=" + studentId +
+                ", studentName='" + studentName + '\'' +
+                '}';
+    }
 }
