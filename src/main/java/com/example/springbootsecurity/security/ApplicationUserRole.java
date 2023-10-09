2bf4666132565e1285498f8690061214c4af2055
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-10-09 11:01:59 +0300

    adding authorities to users

--- src/main/java/com/example/springbootsecurity/security/ApplicationUserRole.java
+++ src/main/java/com/example/springbootsecurity/security/ApplicationUserRole.java
@@ -1,8 +1,10 @@
 package com.example.springbootsecurity.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.example.springbootsecurity.security.ApplicationUserPermission.*;
 
@@ -18,6 +20,13 @@ public enum ApplicationUserRole {
     }
 
     public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
         return permissions;
     }
 }
