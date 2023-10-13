ac003b03eb340e63cc63cbb4a571c35fcc36d833
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-10-13 16:09:12 +0300

    logout

--- src/main/java/com/example/springbootsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springbootsecurity/security/ApplicationSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
 
@@ -57,7 +58,17 @@ public class ApplicationSecurityConfig {
                   .and()
                   .rememberMe()
                   .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
-                  .key("somethingverysecured");
+                  .key("somethingverysecured")
+                  .rememberMeParameter("remember-me")
+                  .and()
+                  .logout()
+                  .logoutUrl("/logout")
+                  .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET")) // https://docs.spring.io/spring-security/site/docs/4.2.12.RELEASE/apidocs/org/springframework/security/config/annotation/web/configurers/LogoutConfigurer.html
+                  .clearAuthentication(true)
+                  .invalidateHttpSession(true)
+                  .deleteCookies("JSESSIONID", "remember-me")
+                  .logoutSuccessUrl("/login");
+
 //                  .httpBasic(withDefaults());
           return http.build();
       }
